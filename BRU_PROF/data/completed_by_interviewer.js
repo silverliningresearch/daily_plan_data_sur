1f344d8b509cf0c811e5c07785915615c77c7cb6
unknown <lb@silverliningresearch.com>
2023-04-14 22:56:03 +0000

    update new data

--- BRU_PROF/data/completed_by_interviewer.js
+++ BRU_PROF/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "14-04-2023 23:44:16"
+        "download_time": "15-04-2023 00:56:03"
     },
     {
         "InterviewerID": 1,
--- BRU_PROF/data/interview_data.js
+++ BRU_PROF/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "LHR",
         "AirlineCode": "",
         "InterviewerID": 4,
-        "download_time": "14-04-2023 23:44:16"
+        "download_time": "15-04-2023 00:56:03"
     },
     {
         "InterviewId": "3502be38-b884-4fc9-83ec-7aa0855fdde4",
