6de5cfddbccd01baf26e39f49659e44984e18005
lexuanbinh <binh.le.xuan@gmail.com>
2023-04-14 07:30:32 +0700

    update new data

--- BRU_PROF/data/completed_by_interviewer.js
+++ BRU_PROF/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-04-03",
         "completed_interviews": 51,
-        "download_time": "14-04-2023 01:29:33"
+        "download_time": "14-04-2023 02:30:13"
     },
     {
         "InterviewerID": 1,
--- BRU_PROF/data/interview_data.js
+++ BRU_PROF/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "LHR",
         "AirlineCode": "",
         "InterviewerID": 4,
-        "download_time": "14-04-2023 01:29:33"
+        "download_time": "14-04-2023 02:30:13"
     },
     {
         "InterviewId": "3502be38-b884-4fc9-83ec-7aa0855fdde4",
