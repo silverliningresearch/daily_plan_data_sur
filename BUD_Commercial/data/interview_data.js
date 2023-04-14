9c5dd4f2ba655fa64b61fedf110088b90bf8ced1
unknown <lb@silverliningresearch.com>
2023-04-14 23:23:06 +0000

    update new data

--- BUD_Commercial/data/interview_data.js
+++ BUD_Commercial/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "MXP",
         "AirlineCode": "W6",
         "InterviewerID": 7,
-        "download_time": "15-04-2023 01:12:42"
+        "download_time": "15-04-2023 01:23:06"
     },
     {
         "InterviewId": "611894fe-2f78-4f05-8286-e9cc966385f9",
