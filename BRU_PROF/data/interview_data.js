2c69ca15f0a63dc0bc6fc5f08ccfa489fd1ad774
lexuanbinh <binh.le.xuan@gmail.com>
2023-04-15 08:05:33 +0700

    update new data

--- BRU_PROF/data/interview_data.js
+++ BRU_PROF/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "LHR",
         "AirlineCode": "",
         "InterviewerID": 4,
-        "download_time": "15-04-2023 02:56:33"
+        "download_time": "15-04-2023 03:05:12"
     },
     {
         "InterviewId": "3502be38-b884-4fc9-83ec-7aa0855fdde4",
