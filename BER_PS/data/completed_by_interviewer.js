eb1d0f1d32c7440c1d61b2837f275af6bd36146f
unknown <lb@silverliningresearch.com>
2023-04-18 11:55:39 +0000

    update new data

--- BER_PS/data/completed_by_interviewer.js
+++ BER_PS/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-03-31",
         "completed_interviews": 58,
-        "download_time": "18-04-2023 12:55:35"
+        "download_time": "18-04-2023 13:55:39"
     },
     {
         "InterviewerID": 1,
--- BER_PS/data/interview_data.js
+++ BER_PS/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "New York (JFK)",
         "AirlineCode": "Norse Atlantic Airways",
         "InterviewerID": 12,
-        "download_time": "18-04-2023 12:55:35"
+        "download_time": "18-04-2023 13:55:39"
     },
     {
         "InterviewId": "5c09e1d9-c741-4fb7-a9d4-4a198a8dc8f4",
