62b217f1a029983e0ecc34a0b6024a46e75bda57
unknown <lb@silverliningresearch.com>
2023-04-23 20:37:54 +0000

    update new data

--- BER_PS/data/interview_data.js
+++ BER_PS/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "New York (JFK)",
         "AirlineCode": "Norse Atlantic Airways",
         "InterviewerID": 12,
-        "download_time": "23-04-2023 21:38:53"
+        "download_time": "23-04-2023 22:37:53"
     },
     {
         "InterviewId": "5c09e1d9-c741-4fb7-a9d4-4a198a8dc8f4",
