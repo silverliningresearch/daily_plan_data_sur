54afea78d76fa0aaff785aeb918934e6e441f2fd
unknown <lb@silverliningresearch.com>
2023-04-27 16:05:28 +0000

    update new data

--- BER_PS/data/completed_by_interviewer.js
+++ BER_PS/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-03-25",
         "completed_interviews": 56,
-        "download_time": "27-04-2023 17:06:19"
+        "download_time": "27-04-2023 18:05:27"
     },
     {
         "InterviewerID": 1,
--- BER_PS/data/interview_data.js
+++ BER_PS/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "Istanbul (IST)",
         "AirlineCode": "Turkish Airlines",
         "InterviewerID": 12,
-        "download_time": "27-04-2023 17:06:19"
+        "download_time": "27-04-2023 18:05:27"
     },
     {
         "InterviewId": "c471d1e4-99f2-4c65-8cc6-19c72e9664e9",
