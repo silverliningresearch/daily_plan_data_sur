16c4ff0e78d3bcf55e7f7dcf3b12a6c438a9b49d
unknown <lb@silverliningresearch.com>
2023-04-28 23:33:23 +0000

    update new data

--- BER_PS/data/completed_by_interviewer.js
+++ BER_PS/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-03-25",
         "completed_interviews": 56,
-        "download_time": "29-04-2023 00:34:21"
+        "download_time": "29-04-2023 01:33:23"
     },
     {
         "InterviewerID": 1,
--- CAG_AirHub/data/completed_by_interviewer.js
+++ CAG_AirHub/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": "",
         "Interview_Date": "2023-04-01",
         "completed_interviews": 196,
-        "download_time": "29-04-2023 06:33:53"
+        "download_time": "29-04-2023 07:32:52"
     },
     {
         "InterviewerID": "",
