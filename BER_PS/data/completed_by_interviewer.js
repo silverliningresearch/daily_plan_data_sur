c0c7b9c93435379130c764513d3cb4df7e52149e
unknown <lb@silverliningresearch.com>
2023-05-07 21:55:31 +0000

    update new data

--- BER_PS/data/completed_by_interviewer.js
+++ BER_PS/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-03-25",
         "completed_interviews": 56,
-        "download_time": "07-05-2023 22:56:12"
+        "download_time": "07-05-2023 23:55:30"
     },
     {
         "InterviewerID": 1,
