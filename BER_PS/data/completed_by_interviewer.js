4b308e690b5e4bcd0eb48133503b8448342f4744
unknown <lb@silverliningresearch.com>
2023-05-15 20:41:44 +0000

    update new data

--- BER_PS/data/completed_by_interviewer.js
+++ BER_PS/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-03-25",
         "completed_interviews": 56,
-        "download_time": "15-05-2023 21:42:45"
+        "download_time": "15-05-2023 22:41:42"
     },
     {
         "InterviewerID": 1,
--- CPH_Route/data/completed_by_interviewer.js
+++ CPH_Route/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 999,
         "Interview_Date": "2023-05-04",
         "completed_interviews": 3,
-        "download_time": "15-05-2023 21:42:46"
+        "download_time": "15-05-2023 22:41:43"
     },
     {
         "InterviewerID": 999,
