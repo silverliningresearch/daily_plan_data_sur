2d489c70f8378b42936790463577cb907d341c9c
unknown <lb@silverliningresearch.com>
2023-05-16 21:16:18 +0000

    update new data

--- BER_PS/data/interview_data.js
+++ BER_PS/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "Istanbul (IST)",
         "AirlineCode": "Turkish Airlines",
         "InterviewerID": 12,
-        "download_time": "16-05-2023 22:17:15"
+        "download_time": "16-05-2023 23:16:16"
     },
     {
         "InterviewId": "c471d1e4-99f2-4c65-8cc6-19c72e9664e9",
