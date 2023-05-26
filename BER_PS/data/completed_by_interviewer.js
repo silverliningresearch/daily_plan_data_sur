bea5fe007b378467fac0a90a827b3878fd281450
unknown <lb@silverliningresearch.com>
2023-05-26 16:35:11 +0000

    update new data

--- BER_PS/data/completed_by_interviewer.js
+++ BER_PS/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-03-25",
         "completed_interviews": 56,
-        "download_time": "26-05-2023 17:36:11"
+        "download_time": "26-05-2023 18:35:09"
     },
     {
         "InterviewerID": 1,
--- CPH_Route/data/interview_data.js
+++ CPH_Route/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "InterviewerID": 999,
-        "download_time": "26-05-2023 17:36:12"
+        "download_time": "26-05-2023 18:35:11"
     },
     {
         "InterviewId": "53beb8d9-b4ca-42e3-b6c8-2a906a003c33",
