f3580ba5d8677a29c0498b0b266a317ad0cf8676
unknown <lb@silverliningresearch.com>
2023-06-08 03:45:59 +0000

    update new data

---
+++ VIE/data/interview_data.js
@@ -0,0 +1,12 @@
+let interview_data_raw = `[
+    {
+        "InterviewId": "965d167c-8ff2-4293-9236-12a1a0e6ea27",
+        "InterviewEndDate": "2023-06-07 11:27:02",
+        "InterviewState": "Completed",
+        "Flight": "4M 0852",
+        "Dest": "AYT",
+        "download_time": "08-06-2023 05:42:56"
+    }
+]    
+
+ `;
