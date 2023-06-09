a706ff81936f2defa2c15f44ce7d0f3e698eaf4c
unknown <lb@silverliningresearch.com>
2023-06-09 02:08:52 +0000

    update new data

--- BER_PS/data/interview_data.js
+++ BER_PS/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "Funchal (FNC)",
         "AirlineCode": "easyJet Europe",
         "InterviewerID": 11,
-        "download_time": "09-06-2023 03:09:42"
+        "download_time": "09-06-2023 04:08:43"
     },
     {
         "InterviewId": "43d7a983-e57c-4a0c-b16d-042e9d875c97",
--- VIE/data/interview_data.js
+++ VIE/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Flight": "4M 0852",
         "Dest": "AYT",
         "InterviewerID": 999,
-        "download_time": "09-06-2023 03:09:49"
+        "download_time": "09-06-2023 04:08:51"
     },
     {
         "InterviewId": "81f3a919-c4b5-4a24-a644-241f361ee30f",
