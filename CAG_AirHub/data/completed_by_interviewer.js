60ca3196333a8ccfb5eb04b260286a8a59b5801a
unknown <lb@silverliningresearch.com>
2023-06-11 17:04:40 +0000

    update new data

--- CAG_AirHub/data/completed_by_interviewer.js
+++ CAG_AirHub/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": "",
         "Interview_Date": "2023-04-01",
         "completed_interviews": 196,
-        "download_time": "12-06-2023 00:04:38"
+        "download_time": "12-06-2023 01:03:36"
     },
     {
         "InterviewerID": "",
--- VIE/data/interview_data.js
+++ VIE/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Flight": "4M 0852",
         "Dest": "AYT",
         "InterviewerID": 999,
-        "download_time": "11-06-2023 18:05:45"
+        "download_time": "11-06-2023 19:04:40"
     },
     {
         "InterviewId": "965d167c-8ff2-4293-9236-12a1a0e6ea27",
