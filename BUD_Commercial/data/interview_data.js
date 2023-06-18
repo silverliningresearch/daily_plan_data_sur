8ab5e0715aaa819953a6cbfea11f811dc72a6686
unknown <lb@silverliningresearch.com>
2023-06-18 01:30:02 +0000

    update new data

--- BUD_Commercial/data/interview_data.js
+++ BUD_Commercial/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "IST",
         "AirlineCode": "TK",
         "InterviewerID": 7,
-        "download_time": "18-06-2023 02:28:49"
+        "download_time": "18-06-2023 03:27:48"
     },
     {
         "InterviewId": "5ff83009-9fc3-465b-97fb-4fddc230a041",
--- CPH_Route/data/completed_by_interviewer.js
+++ CPH_Route/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-06-01",
         "completed_interviews": 25,
-        "download_time": "18-06-2023 02:30:51"
+        "download_time": "18-06-2023 03:29:57"
     },
     {
         "InterviewerID": 1,
