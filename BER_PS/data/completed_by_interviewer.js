bb84ab49740ee8b1f87ddadde13fe33d90f2416a
unknown <lb@silverliningresearch.com>
2023-06-23 22:25:57 +0000

    update new data

--- BER_PS/data/completed_by_interviewer.js
+++ BER_PS/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-03-25",
         "completed_interviews": 56,
-        "download_time": "23-06-2023 23:26:38"
+        "download_time": "24-06-2023 00:25:40"
     },
     {
         "InterviewerID": 1,
--- VIE/data/interview_data.js
+++ VIE/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Flight": "4M 0852",
         "Dest": "AYT",
         "InterviewerID": 999,
-        "download_time": "23-06-2023 23:26:53"
+        "download_time": "24-06-2023 00:25:57"
     },
     {
         "InterviewId": "07ef4cf4-bdf4-42be-8367-a4762271173a",
