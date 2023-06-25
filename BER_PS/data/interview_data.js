640051e18975dd26a5b15a2210a8cf96eee6b831
unknown <lb@silverliningresearch.com>
2023-06-25 20:38:31 +0000

    update new data

--- BER_PS/data/interview_data.js
+++ BER_PS/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "Funchal (FNC)",
         "AirlineCode": "easyJet Europe",
         "InterviewerID": 11,
-        "download_time": "25-06-2023 21:39:18"
+        "download_time": "25-06-2023 22:38:16"
     },
     {
         "InterviewId": "43d7a983-e57c-4a0c-b16d-042e9d875c97",
--- CAG_AirHub/data/completed_by_interviewer.js
+++ CAG_AirHub/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": "",
         "Interview_Date": "2023-04-01",
         "completed_interviews": 196,
-        "download_time": "26-06-2023 03:38:40"
+        "download_time": "26-06-2023 04:37:38"
     },
     {
         "InterviewerID": "",
