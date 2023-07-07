80d9e066fe0f29b242966b33ac3b5bb46bc67e7c
unknown <lb@silverliningresearch.com>
2023-07-07 18:53:57 +0000

    update new data

--- BER_PS/data/interview_data.js
+++ BER_PS/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "Funchal (FNC)",
         "AirlineCode": "easyJet Europe",
         "InterviewerID": 11,
-        "download_time": "07-07-2023 19:57:22"
+        "download_time": "07-07-2023 20:53:32"
     },
     {
         "InterviewId": "43d7a983-e57c-4a0c-b16d-042e9d875c97",
