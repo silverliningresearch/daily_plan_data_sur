09533e75c19b70bc29e6167ab8019e5c8f703ba6
unknown <lb@silverliningresearch.com>
2023-07-10 15:39:15 +0000

    update new data

--- VIE/data/interview_data.js
+++ VIE/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Flight": "4M 0852",
         "Dest": "AYT",
         "InterviewerID": 999,
-        "download_time": "10-07-2023 16:43:54"
+        "download_time": "10-07-2023 17:39:13"
     },
     {
         "InterviewId": "965d167c-8ff2-4293-9236-12a1a0e6ea27",
