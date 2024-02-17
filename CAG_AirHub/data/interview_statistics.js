de60180c59c3fc7217cbca14cc72a007718e13d7
unknown <lb@silverliningresearch.com>
2024-02-17 01:37:23 +0000

    update new data

--- CAG_AirHub/data/interview_statistics.js
+++ CAG_AirHub/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AKL",
         "AirlineCode": "NZ",
         "completed_interviews": 6,
-        "download_time": "17-02-2024 08:41:17"
+        "download_time": "17-02-2024 09:37:09"
     },
     {
         "Interview_Date": "2023-04-01",
