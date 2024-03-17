a7acf3f27551295cc25d245e02e38f9ef11a7ee3
unknown <lb@silverliningresearch.com>
2024-03-17 06:51:21 +0000

    update new data

--- BUD_Commercial/data/interview_data.js
+++ BUD_Commercial/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "IST",
         "AirlineCode": "TK",
         "InterviewerID": 7,
-        "download_time": "17-03-2024 06:55:59"
+        "download_time": "17-03-2024 07:50:58"
     },
     {
         "InterviewId": "3a098953-ee22-4aa2-90f8-3090e371cff3",
