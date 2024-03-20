29c409154a382eaf35dd0aca23696cd2330c9d15
unknown <lb@silverliningresearch.com>
2024-03-20 20:06:29 +0000

    update new data

--- CAG_AirHub/data/completed_by_interviewer.js
+++ CAG_AirHub/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2024-02-07",
         "completed_interviews": 1,
-        "download_time": "21-03-2024 03:11:18"
+        "download_time": "21-03-2024 04:06:20"
     },
     {
         "InterviewerID": 2,
--- CPH_Route/data/interview_statistics.js
+++ CPH_Route/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "ARN",
         "AirlineCode": "SK",
         "completed_interviews": 9,
-        "download_time": "20-03-2024 20:11:24"
+        "download_time": "20-03-2024 21:06:28"
     },
     {
         "Interview_Date": "2023-06-01",
