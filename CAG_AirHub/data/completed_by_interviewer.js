7e003ef864ad1770a629ff15d3502cc33d184855
unknown <lb@silverliningresearch.com>
2024-03-30 07:26:58 +0000

    update new data

--- CAG_AirHub/data/completed_by_interviewer.js
+++ CAG_AirHub/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2024-02-07",
         "completed_interviews": 1,
-        "download_time": "30-03-2024 14:31:23"
+        "download_time": "30-03-2024 15:26:18"
     },
     {
         "InterviewerID": 2,
--- CAG_AirHub/data/interview_statistics.js
+++ CAG_AirHub/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AKL",
         "AirlineCode": "NZ",
         "completed_interviews": 6,
-        "download_time": "30-03-2024 14:31:23"
+        "download_time": "30-03-2024 15:26:18"
     },
     {
         "Interview_Date": "2023-04-01",
