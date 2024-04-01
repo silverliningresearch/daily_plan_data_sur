e7e446ca41583e5edc1ce927dd34aee59edacd0c
unknown <lb@silverliningresearch.com>
2024-04-01 13:31:23 +0000

    update new data

--- CAG_AirHub/data/completed_by_interviewer.js
+++ CAG_AirHub/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2024-02-07",
         "completed_interviews": 1,
-        "download_time": "01-04-2024 20:36:11"
+        "download_time": "01-04-2024 21:31:08"
     },
     {
         "InterviewerID": 2,
@@ -763,7 +763,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 6,
         "Interview_Date": "2024-04-01",
-        "completed_interviews": 30
+        "completed_interviews": 35
     },
     {
         "InterviewerID": 7,
@@ -958,7 +958,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 7,
         "Interview_Date": "2024-04-01",
-        "completed_interviews": 39
+        "completed_interviews": 49
     },
     {
         "InterviewerID": 14,
--- CAG_AirHub/data/interview_statistics.js
+++ CAG_AirHub/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AKL",
         "AirlineCode": "NZ",
         "completed_interviews": 6,
-        "download_time": "01-04-2024 20:36:11"
+        "download_time": "01-04-2024 21:31:08"
     },
     {
         "Interview_Date": "2023-04-01",
@@ -33242,6 +33242,12 @@ let interview_statistics = `[
     },
     {
         "Interview_Date": "2024-04-01",
+        "Dest": "BNE",
+        "AirlineCode": "SQ",
+        "completed_interviews": 5
+    },
+    {
+        "Interview_Date": "2024-04-01",
         "Dest": "CGK",
         "AirlineCode": "3K",
         "completed_interviews": 5
@@ -33328,13 +33334,19 @@ let interview_statistics = `[
         "Interview_Date": "2024-04-01",
         "Dest": "PEN",
         "AirlineCode": "AK",
-        "completed_interviews": 8
+        "completed_interviews": 14
     },
     {
         "Interview_Date": "2024-04-01",
         "Dest": "PKU",
         "AirlineCode": "TR",
         "completed_interviews": 3
+    },
+    {
+        "Interview_Date": "2024-04-01",
+        "Dest": "PVG",
+        "AirlineCode": "9C",
+        "completed_interviews": 4
     },
     {
         "Interview_Date": "2024-04-01",
