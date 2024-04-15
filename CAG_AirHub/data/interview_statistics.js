1c54bf31b63a536276a6bea9fa314a0d2296e74e
unknown <lb@silverliningresearch.com>
2024-04-15 21:16:28 +0000

    update new data

--- CAG_AirHub/data/interview_statistics.js
+++ CAG_AirHub/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BWN",
         "AirlineCode": "BI",
         "completed_interviews": 1,
-        "download_time": "16-04-2024 04:21:11"
+        "download_time": "16-04-2024 05:16:09"
     },
     {
         "Interview_Date": "2023-01-16",
@@ -41981,6 +41981,12 @@ let interview_statistics = `[
         "Dest": "XMN",
         "AirlineCode": "MF",
         "completed_interviews": 6
+    },
+    {
+        "Interview_Date": "2024-04-16",
+        "Dest": "CAN",
+        "AirlineCode": "TR",
+        "completed_interviews": 7
     },
     {
         "Interview_Date": "2024-04-16",
