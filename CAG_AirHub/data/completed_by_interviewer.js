022e57d03241fdd85a98ef3c45a80eab787feba5
unknown <lb@silverliningresearch.com>
2024-06-03 01:13:30 +0000

    update new data

--- CAG_AirHub/data/completed_by_interviewer.js
+++ CAG_AirHub/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-01-26",
         "completed_interviews": 2,
-        "download_time": "03-06-2024 08:16:24"
+        "download_time": "03-06-2024 09:12:41"
     },
     {
         "InterviewerID": 1,
@@ -3403,7 +3403,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 4,
         "Interview_Date": "2024-06-03",
-        "completed_interviews": 3
+        "completed_interviews": 12
     },
     {
         "InterviewerID": 5,
--- CAG_AirHub/data/interview_statistics.js
+++ CAG_AirHub/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BWN",
         "AirlineCode": "BI",
         "completed_interviews": 1,
-        "download_time": "03-06-2024 08:16:24"
+        "download_time": "03-06-2024 09:12:41"
     },
     {
         "Interview_Date": "2023-01-16",
@@ -45773,6 +45773,12 @@ let interview_statistics = `[
         "Dest": "HKG",
         "AirlineCode": "SQ",
         "completed_interviews": 7
+    },
+    {
+        "Interview_Date": "2024-06-03",
+        "Dest": "PER",
+        "AirlineCode": "SQ",
+        "completed_interviews": 9
     }
 ]    
 
