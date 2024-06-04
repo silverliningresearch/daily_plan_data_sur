cae60cbff15e45a01a307386adea1cb3540d738c
unknown <lb@silverliningresearch.com>
2024-06-04 05:37:10 +0000

    update new data

--- CAG_AirHub/data/completed_by_interviewer.js
+++ CAG_AirHub/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-01-26",
         "completed_interviews": 2,
-        "download_time": "04-06-2024 12:41:33"
+        "download_time": "04-06-2024 13:36:36"
     },
     {
         "InterviewerID": 1,
@@ -1393,7 +1393,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 2,
         "Interview_Date": "2024-06-04",
-        "completed_interviews": 16
+        "completed_interviews": 19
     },
     {
         "InterviewerID": 3,
@@ -3413,7 +3413,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 4,
         "Interview_Date": "2024-06-04",
-        "completed_interviews": 31
+        "completed_interviews": 34
     },
     {
         "InterviewerID": 5,
--- CAG_AirHub/data/interview_statistics.js
+++ CAG_AirHub/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BWN",
         "AirlineCode": "BI",
         "completed_interviews": 1,
-        "download_time": "04-06-2024 12:41:33"
+        "download_time": "04-06-2024 13:36:36"
     },
     {
         "Interview_Date": "2023-01-16",
@@ -45926,9 +45926,15 @@ let interview_statistics = `[
     },
     {
         "Interview_Date": "2024-06-04",
+        "Dest": "KUL",
+        "AirlineCode": "3K",
+        "completed_interviews": 3
+    },
+    {
+        "Interview_Date": "2024-06-04",
         "Dest": "LGK",
         "AirlineCode": "TR",
-        "completed_interviews": 2
+        "completed_interviews": 4
     },
     {
         "Interview_Date": "2024-06-04",
@@ -45941,6 +45947,12 @@ let interview_statistics = `[
         "Dest": "NRT",
         "AirlineCode": "NQ",
         "completed_interviews": 5
+    },
+    {
+        "Interview_Date": "2024-06-04",
+        "Dest": "PEN",
+        "AirlineCode": "TR",
+        "completed_interviews": 1
     },
     {
         "Interview_Date": "2024-06-04",
