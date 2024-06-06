6003ac7ef4113c7d8e57669e3bd99944c1d75aa0
unknown <lb@silverliningresearch.com>
2024-06-06 00:42:05 +0000

    update new data

--- CAG_AirHub/data/completed_by_interviewer.js
+++ CAG_AirHub/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-01-26",
         "completed_interviews": 2,
-        "download_time": "06-06-2024 07:46:54"
+        "download_time": "06-06-2024 08:41:32"
     },
     {
         "InterviewerID": 1,
@@ -1403,7 +1403,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 2,
         "Interview_Date": "2024-06-06",
-        "completed_interviews": 3
+        "completed_interviews": 11
     },
     {
         "InterviewerID": 3,
@@ -3433,7 +3433,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 4,
         "Interview_Date": "2024-06-06",
-        "completed_interviews": 5
+        "completed_interviews": 14
     },
     {
         "InterviewerID": 5,
--- CAG_AirHub/data/interview_statistics.js
+++ CAG_AirHub/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BWN",
         "AirlineCode": "BI",
         "completed_interviews": 1,
-        "download_time": "06-06-2024 07:46:53"
+        "download_time": "06-06-2024 08:41:32"
     },
     {
         "Interview_Date": "2023-01-16",
@@ -46190,6 +46190,24 @@ let interview_statistics = `[
     },
     {
         "Interview_Date": "2024-06-06",
+        "Dest": "BKK",
+        "AirlineCode": "SQ",
+        "completed_interviews": 1
+    },
+    {
+        "Interview_Date": "2024-06-06",
+        "Dest": "DEL",
+        "AirlineCode": "AI",
+        "completed_interviews": 6
+    },
+    {
+        "Interview_Date": "2024-06-06",
+        "Dest": "PEN",
+        "AirlineCode": "TR",
+        "completed_interviews": 9
+    },
+    {
+        "Interview_Date": "2024-06-06",
         "Dest": "RGN",
         "AirlineCode": "SQ",
         "completed_interviews": 3
@@ -46199,6 +46217,12 @@ let interview_statistics = `[
         "Dest": "SUB",
         "AirlineCode": "TR",
         "completed_interviews": 5
+    },
+    {
+        "Interview_Date": "2024-06-06",
+        "Dest": "YVR",
+        "AirlineCode": "AC",
+        "completed_interviews": 1
     }
 ]    
 
