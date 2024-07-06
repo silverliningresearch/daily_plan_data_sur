0a63767a477769e94714547c259a49e1f4405f6f
unknown <lb@silverliningresearch.com>
2024-07-06 10:20:52 +0000

    update new data

--- BUD_Commercial/data/interview_data.js
+++ BUD_Commercial/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "KEF",
         "AirlineCode": "W6",
         "InterviewerID": 1,
-        "download_time": "06-07-2024 11:24:45"
+        "download_time": "06-07-2024 12:19:48"
     },
     {
         "InterviewId": "a62663f8-574c-466e-9cf5-58c788c60e77",
