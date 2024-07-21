b80ecdcff3c1eb1811092fd243dc3a17c8b47c76
unknown <lb@silverliningresearch.com>
2024-07-21 10:35:57 +0000

    update new data

--- CAG_AirHub/data/interview_statistics.js
+++ CAG_AirHub/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BWN",
         "AirlineCode": "BI",
         "completed_interviews": 1,
-        "download_time": "21-07-2024 17:40:14"
+        "download_time": "21-07-2024 18:35:16"
     },
     {
         "Interview_Date": "2023-01-16",
