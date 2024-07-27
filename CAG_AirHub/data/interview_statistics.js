8a8ae537002e4899fe3dcb80d19ffb3350855104
unknown <lb@silverliningresearch.com>
2024-07-27 15:05:35 +0000

    update new data

--- CAG_AirHub/data/interview_statistics.js
+++ CAG_AirHub/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BWN",
         "AirlineCode": "BI",
         "completed_interviews": 1,
-        "download_time": "27-07-2024 22:09:59"
+        "download_time": "27-07-2024 23:05:04"
     },
     {
         "Interview_Date": "2023-01-16",
--- CPH_Route/data/interview_statistics.js
+++ CPH_Route/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 3,
-        "download_time": "27-07-2024 16:10:37"
+        "download_time": "27-07-2024 17:05:31"
     },
     {
         "Interview_Date": "2023-05-28",
@@ -28024,7 +28024,7 @@ let interview_statistics = `[
         "Interview_Date": "2024-07-27",
         "Dest": "AMS",
         "AirlineCode": "SK",
-        "completed_interviews": 8
+        "completed_interviews": 11
     },
     {
         "Interview_Date": "2024-07-27",
@@ -28067,6 +28067,18 @@ let interview_statistics = `[
         "Dest": "ORD",
         "AirlineCode": "SK",
         "completed_interviews": 3
+    },
+    {
+        "Interview_Date": "2024-07-27",
+        "Dest": "STN",
+        "AirlineCode": "FR",
+        "completed_interviews": 18
+    },
+    {
+        "Interview_Date": "2024-07-27",
+        "Dest": "VIE",
+        "AirlineCode": "OS",
+        "completed_interviews": 1
     }
 ]    
 
