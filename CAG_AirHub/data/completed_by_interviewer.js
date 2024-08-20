58dda710ceea2f6257b6b7c76d7f1ea4fbdab076
unknown <lb@silverliningresearch.com>
2024-08-20 13:52:04 +0000

    update new data

--- CAG_AirHub/data/completed_by_interviewer.js
+++ CAG_AirHub/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-01-26",
         "completed_interviews": 2,
-        "download_time": "20-08-2024 20:56:06"
+        "download_time": "20-08-2024 21:51:19"
     },
     {
         "InterviewerID": 1,
@@ -7113,7 +7113,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 7,
         "Interview_Date": "2024-08-20",
-        "completed_interviews": 49
+        "completed_interviews": 56
     },
     {
         "InterviewerID": 8,
@@ -7463,7 +7463,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": 14,
         "Interview_Date": "2024-08-20",
-        "completed_interviews": 11
+        "completed_interviews": 17
     },
     {
         "InterviewerID": "",
--- CAG_AirHub/data/interview_statistics.js
+++ CAG_AirHub/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BWN",
         "AirlineCode": "BI",
         "completed_interviews": 1,
-        "download_time": "20-08-2024 20:56:05"
+        "download_time": "20-08-2024 21:51:19"
     },
     {
         "Interview_Date": "2023-01-16",
@@ -53488,7 +53488,7 @@ let interview_statistics = `[
         "Interview_Date": "2024-08-20",
         "Dest": "HEL",
         "AirlineCode": "AY",
-        "completed_interviews": 2
+        "completed_interviews": 4
     },
     {
         "Interview_Date": "2024-08-20",
@@ -53506,7 +53506,7 @@ let interview_statistics = `[
         "Interview_Date": "2024-08-20",
         "Dest": "ICN",
         "AirlineCode": "KE",
-        "completed_interviews": 1
+        "completed_interviews": 8
     },
     {
         "Interview_Date": "2024-08-20",
@@ -53519,6 +53519,12 @@ let interview_statistics = `[
         "Dest": "JFK",
         "AirlineCode": "SQ",
         "completed_interviews": 7
+    },
+    {
+        "Interview_Date": "2024-08-20",
+        "Dest": "MNL",
+        "AirlineCode": "PR",
+        "completed_interviews": 4
     },
     {
         "Interview_Date": "2024-08-20",
