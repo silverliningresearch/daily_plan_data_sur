caffe588071f8870111eb9238e966d206aeb70fd
unknown <lb@silverliningresearch.com>
2024-09-04 18:41:19 +0000

    update new data

--- BUD_Commercial/data/interview_data.js
+++ BUD_Commercial/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "AYT",
         "AirlineCode": "W62273",
         "InterviewerID": 16,
-        "download_time": "04-09-2024 19:44:53"
+        "download_time": "04-09-2024 20:39:54"
     },
     {
         "InterviewId": "a62663f8-574c-466e-9cf5-58c788c60e77",
--- CPH_Route/data/interview_statistics.js
+++ CPH_Route/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 3,
-        "download_time": "04-09-2024 19:46:26"
+        "download_time": "04-09-2024 20:41:18"
     },
     {
         "Interview_Date": "2023-05-28",
@@ -30688,7 +30688,7 @@ let interview_statistics = `[
         "Interview_Date": "2024-09-04",
         "Dest": "DEL",
         "AirlineCode": "AI",
-        "completed_interviews": 3
+        "completed_interviews": 17
     },
     {
         "Interview_Date": "2024-09-04",
@@ -30712,7 +30712,7 @@ let interview_statistics = `[
         "Interview_Date": "2024-09-04",
         "Dest": "LHR",
         "AirlineCode": "SK",
-        "completed_interviews": 17
+        "completed_interviews": 18
     },
     {
         "Interview_Date": "2024-09-04",
