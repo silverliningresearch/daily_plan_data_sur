3b233a2f2b9f810d750aff0aed1da95b0f0d961c
unknown <lb@silverliningresearch.com>
2024-09-06 02:46:16 +0000

    update new data

--- BUD_Commercial/data/interview_data.js
+++ BUD_Commercial/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "AYT",
         "AirlineCode": "W62273",
         "InterviewerID": 16,
-        "download_time": "06-09-2024 03:50:54"
+        "download_time": "06-09-2024 04:44:51"
     },
     {
         "InterviewId": "a62663f8-574c-466e-9cf5-58c788c60e77",
@@ -22497,6 +22497,24 @@ let interview_data_raw = `[
         "InterviewState": "Completed",
         "Flight": "W6 2221",
         "Dest": "LGW",
+        "AirlineCode": "W6",
+        "InterviewerID": 1
+    },
+    {
+        "InterviewId": "62537a50-5764-4e92-9be2-ed2d6320cfb8",
+        "InterviewEndDate": "2024-09-06 05:30:55",
+        "InterviewState": "Completed",
+        "Flight": "W6 2385",
+        "Dest": "VLC",
+        "AirlineCode": "W6",
+        "InterviewerID": 1
+    },
+    {
+        "InterviewId": "7c045c24-ff8d-465c-b02a-f09e121baaab",
+        "InterviewEndDate": "2024-09-06 05:40:37",
+        "InterviewState": "Completed",
+        "Flight": "W6 2385",
+        "Dest": "VLC",
         "AirlineCode": "W6",
         "InterviewerID": 1
     }
