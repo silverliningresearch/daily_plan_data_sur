fb940a7348ea39f00275d275e281f69d04bd4b47
unknown <lb@silverliningresearch.com>
2024-09-17 09:10:57 +0000

    update new data

--- CPH_Route/data/interview_statistics.js
+++ CPH_Route/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 3,
-        "download_time": "17-09-2024 10:18:42"
+        "download_time": "17-09-2024 11:10:56"
     },
     {
         "Interview_Date": "2023-05-28",
@@ -31388,9 +31388,21 @@ let interview_statistics = `[
     },
     {
         "Interview_Date": "2024-09-17",
+        "Dest": "ALC",
+        "AirlineCode": "D8",
+        "completed_interviews": 9
+    },
+    {
+        "Interview_Date": "2024-09-17",
         "Dest": "AMS",
         "AirlineCode": "EJU",
         "completed_interviews": 9
+    },
+    {
+        "Interview_Date": "2024-09-17",
+        "Dest": "AMS",
+        "AirlineCode": "KL",
+        "completed_interviews": 12
     },
     {
         "Interview_Date": "2024-09-17",
@@ -31415,6 +31427,12 @@ let interview_statistics = `[
         "Dest": "IST",
         "AirlineCode": "TK",
         "completed_interviews": 19
+    },
+    {
+        "Interview_Date": "2024-09-17",
+        "Dest": "LIS",
+        "AirlineCode": "D8",
+        "completed_interviews": 9
     },
     {
         "Interview_Date": "2024-09-17",
