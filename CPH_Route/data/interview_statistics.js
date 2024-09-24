ea3086426761e85e23b992e6433681c8548672ea
unknown <lb@silverliningresearch.com>
2024-09-24 13:31:08 +0000

    update new data

--- CPH_Route/data/interview_statistics.js
+++ CPH_Route/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 3,
-        "download_time": "24-09-2024 14:36:41"
+        "download_time": "24-09-2024 15:31:07"
     },
     {
         "Interview_Date": "2023-05-28",
@@ -31964,6 +31964,12 @@ let interview_statistics = `[
     },
     {
         "Interview_Date": "2024-09-24",
+        "Dest": "ARN",
+        "AirlineCode": "SK",
+        "completed_interviews": 11
+    },
+    {
+        "Interview_Date": "2024-09-24",
         "Dest": "ATL",
         "AirlineCode": "SK",
         "completed_interviews": 9
@@ -31979,6 +31985,18 @@ let interview_statistics = `[
         "Dest": "BKK",
         "AirlineCode": "TG",
         "completed_interviews": 28
+    },
+    {
+        "Interview_Date": "2024-09-24",
+        "Dest": "CDG",
+        "AirlineCode": "EJU",
+        "completed_interviews": 15
+    },
+    {
+        "Interview_Date": "2024-09-24",
+        "Dest": "DOH",
+        "AirlineCode": "AY",
+        "completed_interviews": 5
     },
     {
         "Interview_Date": "2024-09-24",
@@ -32020,7 +32038,7 @@ let interview_statistics = `[
         "Interview_Date": "2024-09-24",
         "Dest": "OSL",
         "AirlineCode": "SK",
-        "completed_interviews": 6
+        "completed_interviews": 9
     },
     {
         "Interview_Date": "2024-09-24",
