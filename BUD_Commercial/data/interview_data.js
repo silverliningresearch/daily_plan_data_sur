bf0782387c1e71f0bb666449917d1c0b3b19ccfe
unknown <lb@silverliningresearch.com>
2024-09-26 11:23:21 +0000

    update new data

--- BUD_Commercial/data/interview_data.js
+++ BUD_Commercial/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "KEF",
         "AirlineCode": "W6",
         "InterviewerID": 1,
-        "download_time": "26-09-2024 12:26:07"
+        "download_time": "26-09-2024 13:20:49"
     },
     {
         "InterviewId": "af134b98-c1f1-4352-a6f5-529c16b3169a",
--- CPH_Route/data/interview_statistics.js
+++ CPH_Route/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 3,
-        "download_time": "26-09-2024 12:28:06"
+        "download_time": "26-09-2024 13:23:18"
     },
     {
         "Interview_Date": "2023-05-28",
@@ -32140,7 +32140,7 @@ let interview_statistics = `[
         "Interview_Date": "2024-09-26",
         "Dest": "BOS",
         "AirlineCode": "SK",
-        "completed_interviews": 2
+        "completed_interviews": 11
     },
     {
         "Interview_Date": "2024-09-26",
@@ -32153,6 +32153,12 @@ let interview_statistics = `[
         "Dest": "JFK",
         "AirlineCode": "SK",
         "completed_interviews": 1
+    },
+    {
+        "Interview_Date": "2024-09-26",
+        "Dest": "KEF",
+        "AirlineCode": "FI",
+        "completed_interviews": 3
     },
     {
         "Interview_Date": "2024-09-26",
