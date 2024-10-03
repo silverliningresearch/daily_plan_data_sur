93ad212ad601bb4596f678b628c832a3fb622f3a
unknown <lb@silverliningresearch.com>
2024-10-03 08:24:29 +0000

    update new data

--- CAG_AirHub/data/interview_statistics.js
+++ CAG_AirHub/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "BWN",
         "AirlineCode": "BI",
         "completed_interviews": 1,
-        "download_time": "03-10-2024 15:25:59"
+        "download_time": "03-10-2024 16:23:19"
     },
     {
         "Interview_Date": "2023-01-16",
@@ -56871,6 +56871,12 @@ let interview_statistics = `[
     {
         "Interview_Date": "2024-10-03",
         "Dest": "CGK",
+        "AirlineCode": "ID",
+        "completed_interviews": 5
+    },
+    {
+        "Interview_Date": "2024-10-03",
+        "Dest": "CGK",
         "AirlineCode": "QZ",
         "completed_interviews": 5
     },
@@ -56879,6 +56885,12 @@ let interview_statistics = `[
         "Dest": "CGK",
         "AirlineCode": "TR",
         "completed_interviews": 10
+    },
+    {
+        "Interview_Date": "2024-10-03",
+        "Dest": "DEL",
+        "AirlineCode": "SQ",
+        "completed_interviews": 7
     },
     {
         "Interview_Date": "2024-10-03",
--- CPH_Route/data/interview_statistics.js
+++ CPH_Route/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 3,
-        "download_time": "03-10-2024 09:26:45"
+        "download_time": "03-10-2024 10:24:26"
     },
     {
         "Interview_Date": "2023-05-28",
@@ -32600,9 +32600,21 @@ let interview_statistics = `[
     },
     {
         "Interview_Date": "2024-10-03",
+        "Dest": "LHR",
+        "AirlineCode": "BA",
+        "completed_interviews": 12
+    },
+    {
+        "Interview_Date": "2024-10-03",
+        "Dest": "SIN",
+        "AirlineCode": "SQ",
+        "completed_interviews": 6
+    },
+    {
+        "Interview_Date": "2024-10-03",
         "Dest": "ZRH",
         "AirlineCode": "LX",
-        "completed_interviews": 1
+        "completed_interviews": 6
     }
 ]    
 
