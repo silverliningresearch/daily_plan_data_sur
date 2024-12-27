c81a7c6eda03869e682928f7d982df8837fa610d
unknown <lb@silverliningresearch.com>
2024-12-27 19:14:00 +0000

    update new data

--- CAG_AirHub/data/completed_by_interviewer.js
+++ CAG_AirHub/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-01-26",
         "completed_interviews": 2,
-        "download_time": "28-12-2024 01:11:32"
+        "download_time": "28-12-2024 03:11:40"
     },
     {
         "InterviewerID": 1,
--- CPH_Route/data/interview_statistics.js
+++ CPH_Route/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 3,
-        "download_time": "27-12-2024 18:12:50"
+        "download_time": "27-12-2024 20:13:01"
     },
     {
         "Interview_Date": "2023-05-28",
@@ -37502,6 +37502,12 @@ let interview_statistics = `[
     },
     {
         "Interview_Date": "2024-12-27",
+        "Dest": "ADD",
+        "AirlineCode": "ET",
+        "completed_interviews": 13
+    },
+    {
+        "Interview_Date": "2024-12-27",
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 12
@@ -37564,6 +37570,12 @@ let interview_statistics = `[
         "Interview_Date": "2024-12-27",
         "Dest": "ORD",
         "AirlineCode": "SK",
+        "completed_interviews": 3
+    },
+    {
+        "Interview_Date": "2024-12-27",
+        "Dest": "VIE",
+        "AirlineCode": "ET",
         "completed_interviews": 3
     },
     {
